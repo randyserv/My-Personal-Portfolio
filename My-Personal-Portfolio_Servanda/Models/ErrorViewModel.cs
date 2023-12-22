72976e72d23c869a0355095b069218758d22a2b6
randyserv <rase.servanda.ph@gmail.com>
2023-12-22 20:47:12 +0800

    Initial Commit

---
+++ My-Personal-Portfolio_Servanda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_Personal_Portfolio_Servanda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
